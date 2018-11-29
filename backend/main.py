962d91902b6fc2b35c1941b8518339f85f8db513
Sahir Vellani <sahir.vellani@gmail.com>
2018-11-29 16:16:13 -0500

    updates inventory

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
-from flask import Flask 
+from flask import Flask, request, render_template 
 import json
+import logging
 
 app = Flask(__name__)
+
+inventory = {}
 
 @app.route("/", methods=['GET'])
 def main():
@@ -10,11 +13,29 @@ def main():
 
 @app.route("/inventory", methods=['GET'])
 def get_inventory():
-    return json.dumps({'inventory': 'ayo'}), 200, {'ContentType':'application/json'}
+    return json.dumps(inventory), 200, {'ContentType':'application/json'}
 
 @app.route("/update", methods=['POST'])
 def update_inventory():
-    pass
+    content = json.loads(request.data)
+    food_item = content['queryResult']['parameters']['food']
+    food_quantity = content['queryResult']['parameters']['number']
+    action = content['queryResult']['parameters']['action']
+
+    logging.info(food_item)
+    logging.info(food_quantity)
+    logging.info(action)
+    if food_item in inventory and action == 'insert':
+        new_quantity = inventory[food_item] + food_quantity
+        inventory[food_item] = new_quantity
+    elif food_item in inventory and action == 'remove':
+        new_quantity = inventory[food_item] - food_quantity
+        inventory[food_item] = new_quantity
+    else:
+        inventory[food_item] = food_quantity
+    logging.info(inventory)
+    return json.dumps({'speech': 'Thank you.', 'type': 0}), 200, {'ContentType':'application/json'}
+
 
 
 
